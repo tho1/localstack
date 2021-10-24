177173815643dbe7a809fe24e11e0f95eac1d7ec
Thomas Rausch <thomas@thrau.at>
2021-10-24 18:41:12 +0200

    implement more handlers in sqs provider

--- localstack/services/sqs/provider.py
+++ localstack/services/sqs/provider.py
@@ -1,3 +1,5 @@
+import copy
+import inspect
 import logging
 import random
 import re
@@ -17,10 +19,13 @@ from localstack.aws.api.sqs import (
     Integer,
     InvalidAttributeName,
     ListQueuesResult,
+    ListQueueTagsResult,
     Message,
     MessageAttributeNameList,
     MessageBodyAttributeMap,
     MessageBodySystemAttributeMap,
+    MessageSystemAttributeName,
+    PurgeQueueInProgress,
     QueueAttributeMap,
     QueueAttributeName,
     QueueDoesNotExist,
@@ -82,6 +87,8 @@ class SqsQueue:
     tags: TagMap
     message: List[Message]
 
+    purge_in_progress: bool
+
     def __init__(self, key: QueueKey, attributes=None, tags=None) -> None:
         super().__init__()
         self.key = key
@@ -91,6 +98,8 @@ class SqsQueue:
         self.attributes = self.default_attributes()
         if attributes:
             self.attributes.update(attributes)
+
+        self.purge_in_progress = False
 
     def default_attributes(self) -> QueueAttributeMap:
         return {
@@ -106,6 +115,12 @@ class SqsQueue:
             QueueAttributeName.DelaySeconds: "0",
             QueueAttributeName.ReceiveMessageWaitTimeSeconds: "0",
         }
+
+    def update_last_modified(self, timestamp: int = None):
+        if timestamp is None:
+            timestamp = now()
+
+        self.attributes[QueueAttributeName.LastModifiedTimestamp] = str(timestamp)
 
     @property
     def name(self):
@@ -125,7 +140,17 @@ class SqsQueue:
 
 
 class SqsProvider(SqsApi):
-    # TODO: thread safety
+    """
+    LocalStack SQS Provider.
+
+    LIMITATIONS:
+        - Calculation of message attribute MD5 hash.
+        - VisibilityTimeout
+        - Message deletion
+        - Pagination of results (NextToken)
+        - Sequence numbering
+        - Delivery guarantees
+    """
 
     queues: Dict[QueueKey, SqsQueue]
     queue_url_index: Dict[str, SqsQueue]
@@ -134,16 +159,32 @@ class SqsProvider(SqsApi):
         super().__init__()
         self.queues = dict()
         self.queue_url_index = dict()
+        self._mutex = threading.RLock()
 
     def _add_queue(self, queue: SqsQueue):
-        self.queues[queue.key] = queue
-        self.queue_url_index[queue.url] = queue
+        with self._mutex:
+            self.queues[queue.key] = queue
+            self.queue_url_index[queue.url] = queue
 
     def _remove_queue_by_url(self, queue_url: str):
-        queue = self.queue_url_index[queue_url]
+        with self._mutex:
+            queue = self.queue_url_index[queue_url]
+            del self.queues[queue.key]
+            del self.queue_url_index[queue_url]
 
-        del self.queues[queue.key]
-        del self.queue_url_index[queue_url]
+    def _require_queue_by_url(self, queue_url: str) -> SqsQueue:
+        """
+        Returns the queue for the given url, or raises a NonExistentQueue error.
+
+        :param queue_url: The QueueUrl
+        :returns: the queue
+        :raises NonExistentQueue: if the queue does not exist
+        """
+        with self._mutex:
+            try:
+                return self.queue_url_index[queue_url]
+            except KeyError:
+                raise NonExistentQueue()
 
     @handler("CreateQueue")
     def create_queue(
@@ -188,8 +229,6 @@ class SqsProvider(SqsApi):
         urls = list()
 
         for queue in self.queues.values():
-            LOG.info("queue: %s: %s", queue, queue.key)
-
             if queue.key.region != context.region:
                 continue
             if queue.key.account_id != context.account_id:
@@ -200,28 +239,26 @@ class SqsProvider(SqsApi):
             urls.append(queue.url)
 
         if max_results:
+            # FIXME: also need to solve pagination with stateful iterators: If the total number of items available is
+            #  more than the value specified, a NextToken is provided in the command's output. To resume pagination,
+            #  provide the NextToken value in the starting-token argument of a subsequent command. Do not use the
+            #  NextToken response element directly outside of the AWS CLI.
             urls = urls[:max_results]
 
         return ListQueuesResult(QueueUrls=urls)
 
     @handler("DeleteQueue")
     def delete_queue(self, context: RequestContext, queue_url: String) -> None:
-        if queue_url not in self.queue_url_index:
-            raise NonExistentQueue()
-
-        self._remove_queue_by_url(queue_url)
+        with self._mutex:
+            self._require_queue_by_url(queue_url)
+            self._remove_queue_by_url(queue_url)
 
     @handler("GetQueueAttributes")
     def get_queue_attributes(
         self, context: RequestContext, queue_url: String, attribute_names: AttributeNameList = None
     ) -> GetQueueAttributesResult:
+        queue = self._require_queue_by_url(queue_url)
 
-        if queue_url not in self.queue_url_index:
-            raise NonExistentQueue()
-
-        queue = self.queue_url_index[queue_url]
-
-        LOG.info("getting queue attributes: %s", attribute_names)
         if not attribute_names:
             return GetQueueAttributesResult(Attributes=dict())
 
@@ -252,25 +289,23 @@ class SqsProvider(SqsApi):
         message_deduplication_id: String = None,  # TODO
         message_group_id: String = None,  # TODO
     ) -> SendMessageResult:
+        queue = self._require_queue_by_url(queue_url)
 
-        if queue_url not in self.queue_url_index:
-            # FIXME: cannot return exception that aren't in the spec
-            raise NonExistentQueue()
-
-        queue = self.queue_url_index[queue_url]
+        # TODO: default message attributes (SenderId, ApproximateFirstReceiveTimestamp, ...)
 
         message: Message = Message(
             MessageId=generate_message_id(),
             MD5OfBody=md5(message_body),
             Body=message_body,
-            Attributes=message_system_attributes,
-            MD5OfMessageAttributes=None,
+            Attributes=self._create_message_attributes(context, message_system_attributes),
+            MD5OfMessageAttributes=None,  # TODO (see Message.attribute_md5 from moto)
             MessageAttributes=message_attributes,
         )
 
         delay_seconds = delay_seconds or queue.attributes.get(QueueAttributeName.DelaySeconds, 0)
         if delay_seconds:
-            # FIXME: this is a pretty bad implementation (one thread per message...)
+            # FIXME: this is a pretty bad implementation (one thread per message...). polling on a priority queue
+            #  would probably be better.
             threading.Timer(int(delay_seconds), queue.messages.put_nowait, args=(message,)).start()
         else:
             queue.messages.put_nowait(message)
@@ -295,10 +330,7 @@ class SqsProvider(SqsApi):
         wait_time_seconds: Integer = None,
         receive_request_attempt_id: String = None,
     ) -> ReceiveMessageResult:
-        if queue_url not in self.queue_url_index:
-            raise QueueDoesNotExist("The specified queue does not exist for this wsdl version.")
-
-        queue = self.queue_url_index[queue_url]
+        queue = self._require_queue_by_url(queue_url)
 
         num = max_number_of_messages or 1
         # collect messages
@@ -314,8 +346,96 @@ class SqsProvider(SqsApi):
                     msg = queue.messages.get(timeout=wait_time_seconds)
                 except Empty:
                     break
+
+            # prepare message for receipt
+            msg: Message = copy.deepcopy(msg)
             msg["ReceiptHandle"] = generate_receipt_handle()
+            # filter attributes
+            if message_attribute_names:
+                if "All" not in message_attribute_names:
+                    msg["MessageAttributes"] = {
+                        k: v
+                        for k, v in msg["MessageAttributes"].items()
+                        if k in message_attribute_names
+                    }
+                msg["MD5OfMessageAttributes"] = None  # TODO
+            else:
+                del msg["MessageAttributes"]
+
+            # add message to result
             messages.append(msg)
             num -= 1
 
+        # TODO: how does receiving behave if the queue was deleted in the meantime?
         return ReceiveMessageResult(Messages=messages)
+
+    @handler("PurgeQueue")
+    def purge_queue(self, context: RequestContext, queue_url: String) -> None:
+        queue = self._require_queue_by_url(queue_url)
+
+        with self._mutex:
+            # FIXME: use queue-specific locks
+            if queue.purge_in_progress:
+                raise PurgeQueueInProgress()
+            queue.purge_in_progress = True
+
+        # TODO: how do other methods behave when purge is in progress?
+
+        try:
+            while True:
+                queue.messages.get_nowait()
+        except Empty:
+            return
+        finally:
+            queue.purge_in_progress = False
+
+    @handler("SetQueueAttributes")
+    def set_queue_attributes(
+        self, context: RequestContext, queue_url: String, attributes: QueueAttributeMap
+    ) -> None:
+        queue = self._require_queue_by_url(queue_url)
+
+        if not attributes:
+            return
+
+        self._validate_queue_attributes(attributes)
+
+        for k, v in attributes.items():
+            queue.attributes[k] = v
+
+    @handler("TagQueue")
+    def tag_queue(self, context: RequestContext, queue_url: String, tags: TagMap) -> None:
+        queue = self._require_queue_by_url(queue_url)
+
+        if not tags:
+            return
+
+        for k, v in tags.items():
+            queue.tags[k] = v
+
+    @handler("ListQueueTags")
+    def list_queue_tags(self, context: RequestContext, queue_url: String) -> ListQueueTagsResult:
+        queue = self._require_queue_by_url(queue_url)
+        return ListQueueTagsResult(Tags=queue.tags)
+
+    def _create_message_attributes(
+        self,
+        context: RequestContext,
+        message_system_attributes: MessageBodySystemAttributeMap = None,
+    ) -> Dict[MessageSystemAttributeName, str]:
+        result: Dict[MessageSystemAttributeName, str] = {
+            MessageSystemAttributeName.SenderId: context.account_id,
+            MessageSystemAttributeName.SentTimestamp: str(now()),
+        }
+
+        if message_system_attributes is not None:
+            result.update(message_system_attributes)
+
+        return result
+
+    def _validate_queue_attributes(self, attributes: QueueAttributeMap):
+        valid = [k[1] for k in inspect.getmembers(QueueAttributeName)]
+
+        for k in attributes.keys():
+            if k not in valid:
+                raise InvalidAttributeName("Unknown attribute name %s" % k)
